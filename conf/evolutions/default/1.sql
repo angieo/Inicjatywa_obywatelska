9dab18a8a8645f09f094a022dd1bc5ea35c1d890
Angelika Olejniczak <anesqo@gmail.com>
2013-03-02 00:23:14 +0100

    evolutions again

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,9 +1,11 @@
 
 # --- !Ups
 
-CREATE TABLE citizen(id bigint(20) NOT NULL AUTO_INCREMENT, firstname varchar(255) NOT NULL, lastname varchar(255) NOT NULL, address varchar(255) NOT NULL, pesel varchar(11) NOT NULL, latlng varchar(255) NOT NULL, sigpoint boolean NOT NULL, PRIMARY KEY (id));
+CREATE SEQUENCE citizen_id_seq;
+CREATE TABLE citizen(id integer NOT NULL DEFAULT nextval('citizen_id_seq'), firstname varchar(255) NOT NULL, lastname varchar(255) NOT NULL, address varchar(255) NOT NULL, pesel varchar(11) NOT NULL, latlng varchar(255) NOT NULL, sigpoint boolean NOT NULL, PRIMARY KEY (id));
 
-CREATE TABLE name_gender(id bigint(20) NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, gender varchar(255) NOT NULL, PRIMARY KEY (id)
+CREATE SEQUENCE name_gender_id_seq;
+CREATE TABLE name_gender(id integer NOT NULL DEFAULT nextval('name_gender_id_seq'), name varchar(255) NOT NULL, gender varchar(255) NOT NULL, PRIMARY KEY (id)
 );
 
 # --- !Downs
