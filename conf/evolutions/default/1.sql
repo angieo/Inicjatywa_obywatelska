e8c4b9c3acf966d757869c2593be54fe81755448
Angelika Olejniczak <anesqo@gmail.com>
2013-03-01 23:57:26 +0100

    evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,22 +1,9 @@
 
 # --- !Ups
 
-CREATE TABLE citizen(
-	id bigint(20) NOT NULL AUTO_INCREMENT,
-	firstname varchar(255) NOT NULL,
-	lastname varchar(255) NOT NULL,
-	address varchar(255) NOT NULL,
-	pesel varchar(11) NOT NULL,
-	latlng varchar(255) NOT NULL,
-	sigpoint boolean NOT NULL,
-	PRIMARY KEY (id)
-);
+CREATE TABLE citizen(id bigint(20) NOT NULL AUTO_INCREMENT, firstname varchar(255) NOT NULL, lastname varchar(255) NOT NULL, address varchar(255) NOT NULL, pesel varchar(11) NOT NULL, latlng varchar(255) NOT NULL, sigpoint boolean NOT NULL, PRIMARY KEY (id));
 
-CREATE TABLE name_gender(
-	id bigint(20) NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	gender varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE name_gender(id bigint(20) NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, gender varchar(255) NOT NULL, PRIMARY KEY (id)
 );
 
 # --- !Downs
